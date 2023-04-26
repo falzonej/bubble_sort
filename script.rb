64c2697c572aa2b296f730c09b9e3f4224387eae
JoeFalzone <falzonej@gmail.com>
2023-04-25 22:37:50 -0400

    add check_sorted_array

--- script.rb
+++ script.rb
@@ -1,16 +1,27 @@
 require 'pry'
 
-array = [4,7,2,7,1,5]
+array = [4,5,7,2,5,7,1]
 
 
 def bubble_sort(array)
   output_array = array
-  until array[0] == array.min()  && array[-1] == array.max() do
+  until check_sorted_array(array) do
     output_array = sort_array(array)
   end
   output_array
 end
 
+def check_sorted_array(array)
+  array.each do |num|
+    curr_num = array.find_index(num)
+    next_num = curr_num+1
+    if array[next_num] == nil
+      return true
+    elsif array[curr_num] > array[next_num]
+      return false
+    end
+  end
+end
 
 def sort_array(array)
   array.each do |num|
@@ -30,7 +41,8 @@ def control(array,num1,num2)
   elsif num1 == num2
     curr_num = array.find_index(num1)
     next_num = curr_num + 1
-    control(array, array[curr_num], array[next_num+1])
+    binding.pry
+    control(array, array[curr_num], array[next_num + 1])
   elsif num1 > num2
     switch_arr_index(array,num1,num2) 
   end
@@ -50,3 +62,4 @@ end
 #p switch_arr_index(array,4,7)
 #p sort_array(array)
 p bubble_sort(array)
+#p check_sorted_array([1,2,3])
