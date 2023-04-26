2052fbccfb467c94edfab0af1b7b7b45c12c4006
JoeFalzone <falzonej@gmail.com>
2023-04-25 23:16:51 -0400

    cleaned up finished bubble_sort method

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-array = [4,5,7,2,5,7,1]
+array = [4,1,7,2,5,8,123,5,7,5]
 
 
 def bubble_sort(array)
@@ -12,47 +12,39 @@ def bubble_sort(array)
 end
 
 def check_sorted_array(array)
-  array.each do |num|
-    curr_num = array.find_index(num)
-    next_num = curr_num+1
-    if array[next_num] == nil
+  array.each_with_index do |num, i|
+    if array[i + 1] == nil
       return true
-    elsif array[curr_num] > array[next_num]
+    elsif num > array[i + 1]
       return false
     end
   end
 end
 
 def sort_array(array)
-  array.each do |num|
-    curr_num = array.find_index(num)
-    next_num = curr_num + 1
-    control(array, array[curr_num], array[next_num])
-# binding.pry
+  array.each_with_index do |num, i|
+    control(array, i,i+1)
   end
 end
 
-def control(array,num1,num2)
-# binding.pry
-  if num2 == nil
+def control(array,index1,index2)
+num1 = array[index1]
+num2 = array[index2]
+  if num2 == nil # could've combined the if statements that return immediately with || into one if statement
     return
   elsif num1 < num2 
     return
   elsif num1 == num2
-    curr_num = array.find_index(num1)
-    next_num = curr_num + 1
-    binding.pry
-    control(array, array[curr_num], array[next_num + 1])
+    return
   elsif num1 > num2
-    switch_arr_index(array,num1,num2) 
+    switch_arr_index(array,index1,index2) 
   end
 end
 
-def switch_arr_index(array,num1,num2)
-  num1_index = array.find_index(num1)
-  num2_index = array.find_index(num2)
-  array[num1_index] = num2 
-  array[num2_index] = num1
+def switch_arr_index(array,index1,index2)
+  holder = array[index1]
+  array[index1] = array[index2]
+  array[index2] = holder
   array
 end
 
@@ -61,5 +53,5 @@ end
 #p control(array,7,2)
 #p switch_arr_index(array,4,7)
 #p sort_array(array)
-p bubble_sort(array)
-#p check_sorted_array([1,2,3])
+#p check_sorted_array([1, 2, 4, 5, 7, 7, 5])
+#p bubble_sort(array)
